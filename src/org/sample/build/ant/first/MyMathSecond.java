723f35ab754d209f9a6d2dfa6e102adcc8a8426e
RRastogi <rrastogi69556@gmail.com>
2017-12-17 17:41:13 +0530

    This is the second commit

---
+++ src/org/sample/build/ant/first/MyMathSecond.java
@@ -0,0 +1,7 @@
+package org.sample.build.ant.first;
+
+public class MyMathSecond {
+	public int add(int number1, int number2) {
+		return number1 + number2;
+	}
+}
--- src/org/sample/build/ant/first/test/MyMathTest.java
+++ src/org/sample/build/ant/first/test/MyMathTest.java
@@ -1,10 +1,13 @@
 package org.sample.build.ant.first.test;
 
 import org.sample.build.ant.first.MyMath;
+import org.sample.build.ant.first.MyMathSecond;
 
 public class MyMathTest {
 	public static void main(String[] args) {
         MyMath math = new MyMath();
+        MyMathSecond second=new MyMathSecond();
         System.out.println("Result is: " + math.multi(5, 10));
+        System.out.println("Add is "+second.add(5,10));
     }
 }
